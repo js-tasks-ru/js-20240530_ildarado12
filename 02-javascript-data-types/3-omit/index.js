3a30323f205c0e7eac348b4617eab420e511c82e
Ildar Gataullin <ildarado12@yandex.ru>
2024-06-05 19:57:02 +0300

    Решена 3 задача 2 урока

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,17 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+  const objArr = Object.entries(obj); 
 
+  for (let str of fields) {
+    objArr.map(item => {
+      if (item[0] == str) {
+        objArr.splice(item, 1);
+      }
+    });
+  }  
+  
+  const newObj = Object.fromEntries(objArr);
+  
+  return newObj;
 };
