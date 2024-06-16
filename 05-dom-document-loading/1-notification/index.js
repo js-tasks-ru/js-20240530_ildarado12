af4f86707e58be790291f8c23543978174a07dfc
Ildar Gataullin <ildarado12@yandex.ru>
2024-06-16 22:08:15 +0300

    5 модуль 1 задача не прохит тест

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,51 @@
 export default class NotificationMessage {
+  static currentShowComponent;
 
+  constructor(message = ``, {duration = 1000, type = ``}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+  }
+
+  createElem(template) {
+    const element = document.createElement(`DIV`);
+    element.innerHTML = template;
+    return document.body.append(element.firstElementChild);
+  }
+
+  createShow() {
+    return (`
+      <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+          <div class="timer"></div>
+          <div class="inner-wrapper">
+              <div class="notification-header">${this.type}</div>
+              <div class="notification-body">
+              ${this.message}
+              </div>
+          </div>
+      </div>
+    `);
+  }
+
+  show() {
+    if (NotificationMessage.currentShowComponent) {
+      NotificationMessage.currentShowComponent.hide();
+    }
+    NotificationMessage.currentShowComponent = this;
+
+    this.createElem(this.createShow());
+    
+    const timer = document.querySelector(`.timer`);
+    timer.addEventListener(`animationend`, () => this.hide(), {once: true});
+  }
+
+  hide() {
+    if (document.querySelector(`.notification`)) {
+      document.querySelector(`.notification`).remove();
+    }
+  }
+
+  destroy() {
+    this.hide();
+  }
 }
